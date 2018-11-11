e96dba14709532de75acfe063c12e107eaa10f8b
Trang Tran <tn-trang.tran@outlook.com>
2018-11-11 15:28:25 +0700

    URL Routing: Attribute Routing - Route Prefix and Contraints

--- UrlsAndRoutes/Controllers/CustomerController.cs
+++ UrlsAndRoutes/Controllers/CustomerController.cs
@@ -6,14 +6,26 @@ using System.Web.Mvc;
 
 namespace UrlsAndRoutes.Controllers
 {
+    [RoutePrefix("Users")]
     public class CustomerController : Controller
     {
-        [Route("Test")]
+        [Route("~/Test")]
         public ActionResult Index()
         {
             ViewBag.Controller = "Customer";
             ViewBag.Action = "Index";
             return View("ActionName");
+        }
+        [Route("User/Add/{user}/{id:int}")]
+        public string Create(string user, int id)
+        {
+            return string.Format("Create Method - User: {0}, Id: {1}", user, id);
+        }
+        [Route("User/Add/{user}/{password:alpha:length(6)}")]
+        public string ChangePass(string user, string password)
+        {
+            return string.Format("ChangePass Method - User: {0}, Password: {1}", 
+                user, password);
         }
         public ActionResult List()
         {
